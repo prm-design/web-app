0491edcf9464fa79fd46007c102f823d1c09c09a
Frodo <frodo@MacBookAir.lan>
2026-01-05 19:14:42 +0100

    Add sollevare-una-linea app

--- sollevare-una-linea/vite.config.js
+++ sollevare-una-linea/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+base: "/web-app/sollevare-una-linea/",
+
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
