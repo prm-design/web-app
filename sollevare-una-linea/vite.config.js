d2578c3aac6d18a59bc086883b90dc42c6c2e9ec
prm-design <info@prmdesign.com>
2026-01-05 19:56:58 +0100

    Update vite.config.js

--- sollevare-una-linea/vite.config.js
+++ sollevare-una-linea/vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-base: "/web-app/sollevare-una-linea/",
-
+export default defineConfig({
+  base: "/web-app/sollevare-una-linea/",
+  plugins: [react(), tailwindcss()],
+});
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
